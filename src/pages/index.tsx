7478dc501323de4d8fdf700e1930b2f5391bdb4b
nadam131 <nickadam131@gmail.com>
2021-08-20 13:08:32 +0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <Box>
+      <Heading mt={4} color="#000">
+        Новости
+      </Heading>
+      <Text textStyle="h1">asdasd</Text>
+    </Box>
+  );
+};
+
+export default Home;
