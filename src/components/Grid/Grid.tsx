7a2a630bd3205c814147783697d9db220fc33441
nadam131 <nickadam131@gmail.com>
2021-10-09 22:49:36 +0300

    add inverted to the grid

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -3,23 +3,31 @@ import { Box, Grid, GridItem } from "@chakra-ui/layout";
 interface IGrid {
   children: any;
   aside: any;
+  inverted?: boolean;
 }
 
-export const GridAside = ({ children, aside }: IGrid) => {
+export const GridAside = ({ children, aside, inverted }: IGrid) => {
+  const asideStyles = {
+    py: 6,
+    px: inverted ? 6 : 0,
+    pl: inverted ? 0 : 6,
+    gridColumn: inverted ? "1/span 3" : "span 3",
+    borderRight: inverted ? "1px" : 0,
+    borderLeft: inverted ? 0 : "1px",
+    borderColor: "gray.700",
+  };
+
   return (
-    <Grid h="100vh" gap={6} templateColumns="repeat(12, 1fr)">
-      <GridItem py={6} gridColumn="span 9">
+    <Grid
+      h="100vh"
+      gap={6}
+      gridAutoFlow={inverted ? "dense" : ""}
+      templateColumns="repeat(12, 1fr)"
+    >
+      <GridItem py={6} gridColumn={inverted ? "4/span 9" : "span 9"}>
         {children}
       </GridItem>
-      <GridItem
-        py={6}
-        px={6}
-        gridColumn="span 3"
-        borderLeft="1px"
-        borderColor="gray.700"
-      >
-        {aside}
-      </GridItem>
+      <GridItem {...asideStyles}>{aside}</GridItem>
     </Grid>
   );
 };
