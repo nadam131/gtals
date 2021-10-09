8b50ecac624b313f20c27b3348d57577b4222747
nadam131 <nickadam131@gmail.com>
2021-10-09 22:49:50 +0300

    refactor

--- src/pages/[postType]/[slug].tsx
+++ src/pages/[postType]/[slug].tsx
@@ -9,61 +9,53 @@ import {
   Text,
 } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import useSWR from "swr";
+import useSWR, { SWRConfig } from "swr";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { fetcher } from "../../api";
+import { ENDPOINTS, fetcher } from "../../api";
+import TemplateArticle from "../../components/Template/TemplateArticle";
+import { useFetch } from "../../hooks/use-fetch";
 
-const Single: NextPage = () => {
-  const {
-    pathname,
-    asPath: path,
-    query: { postType },
-  } = useRouter();
+interface ServerSideProps {
+  query: {
+    postType: string;
+    slug: string;
+  };
+}
 
-  const { data, error } = useSWR(
-    `http://localhost:4000/article${path}`,
-    fetcher
-  );
+interface PostPageProps {
+  url: string;
+  fallback: any;
+}
 
-  if (!data) return null;
-
+const PostPage = ({ url, fallback }: PostPageProps) => {
   return (
-    <Container maxW="container.xxl">
-      <Grid h="100vh" gap={10} templateColumns="repeat(12, 1fr)">
-        <GridItem py={10} gridColumn="span 9">
-          <Box>
-            <Heading>{data.title}</Heading>
-          </Box>
-          <Box mt={10}>
-            <Grid templateColumns="repeat(12, 1fr)">
-              {/* <GridItem gridColumn="span 5">
-                <AspectRatio ratio={16 / 9} borderRadius={10} overflow="hidden">
-                  <Image src={data.image} layout="fill" />
-                </AspectRatio>
-              </GridItem> */}
-              <GridItem gridColumn="span 9">
-                <Text>{data.description}</Text>
-              </GridItem>
-            </Grid>
-          </Box>
-          <Box
-            bg="#fafafa"
-            color="black"
-            h="100%"
-            p={10}
-            borderRadius={10}
-            mt={10}
-            dangerouslySetInnerHTML={{ __html: data.content }}
-          ></Box>
-        </GridItem>
-        <GridItem p={10} borderLeft="1px" borderColor="gray.700">
-          <Box>Aside</Box>
-        </GridItem>
-      </Grid>
-    </Container>
+    <SWRConfig
+      value={{
+        fallback,
+      }}
+    >
+      <TemplateArticle url={url} />
+    </SWRConfig>
   );
 };
 
-export default Single;
+export async function getServerSideProps({
+  query: { postType, slug: postSlug },
+}: ServerSideProps) {
+  const url = `${ENDPOINTS.NESTJS}/article/${postType}/${postSlug}`;
+  const post = await fetcher(url);
+
+  return {
+    props: {
+      url,
+      post,
+      fallback: {
+        [`${url}`]: post,
+      },
+    },
+  };
+}
+
+export default PostPage;
