4e09f05413d5b4addd2777cd8ad8d3a8fb236434
nadam131 <nickadam131@gmail.com>
2021-10-26 22:55:21 +0300

    add redirection for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/wp-content/:slug*",
+        destination: "https://gtals.ru/wp-content/:slug*",
+      },
+    ];
+  },
   images: {
     domains: ["gtals.ru", "via.placeholder.com"],
   },
