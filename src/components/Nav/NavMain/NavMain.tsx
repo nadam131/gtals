e8faeab954f175dee595054076a1917b04e1a27d
nadam131 <nickadam131@gmail.com>
2021-10-26 20:47:03 +0300

    add chakra links to NavMain

--- src/components/Nav/NavMain/NavMain.tsx
+++ src/components/Nav/NavMain/NavMain.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, HStack, Link as CLink } from "@chakra-ui/react";
 import Link from "next/link";
 import React from "react";
 
@@ -18,15 +18,16 @@ const NavMain = ({ list }: NavMainProps) => {
     >
       {list.map((item) => {
         return (
-          <Box
-            transition="color .2s"
-            _hover={{
-              color: "primary",
-            }}
-            key={item.id}
-          >
-            <Link href={item.url}>
-              <a>{item.name}</a>
+          <Box transition="color .2s" key={item.id}>
+            <Link href={item.url} passHref>
+              <CLink
+                transition="color .2s"
+                _hover={{
+                  color: "primary.500",
+                }}
+              >
+                {item.name}
+              </CLink>
             </Link>
           </Box>
         );
