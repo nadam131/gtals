998d46909f8d7e93df696f0706f877960bb27013
nadam131 <nickadam131@gmail.com>
2021-11-22 02:04:10 +0300

    refactor

--- src/components/Card/CardCharacter/CardCharacter.tsx
+++ src/components/Card/CardCharacter/CardCharacter.tsx
@@ -1,18 +1,14 @@
-import { Box, Center, Heading, Text, Stack, Image } from "@chakra-ui/react";
+import { Box, Center, Heading, Text, Stack } from "@chakra-ui/react";
+import Image from "../../Image/Image";
 
-const IMAGE =
-  "https://gtals.ru/wp-content/uploads/2015/07/gta5_michael_de_santa_main-1.jpg";
-
-// interface CardCharacterProps {}
-// TODO Доделать
-
-const CardCharacter = () => {
+const CardCharacter = ({ name, image }) => {
   return (
-    <Center py={12}>
+    <Center>
       <Box
         role={"group"}
         p={6}
-        maxW={"330px"}
+        mt={20}
+        maxW={"xs"}
         w={"full"}
         bg={"gray.800"}
         boxShadow={"2xl"}
@@ -20,42 +16,15 @@ const CardCharacter = () => {
         pos={"relative"}
         zIndex={1}
       >
-        <Box
-          rounded={"lg"}
-          mt={-12}
-          pos={"relative"}
-          height={"230px"}
-          _after={{
-            transition: "all .3s ease",
-            content: '""',
-            w: "200px",
-            h: "200px",
-            pos: "absolute",
-            margin: "0 auto",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            borderRadius: "50%",
-            backgroundColor: `#65b4d4`,
-            filter: "blur(15px)",
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: "blur(20px)",
-            },
-          }}
-        >
+        <Box mt={-20} mb={5} pos={"relative"} height={"200px"}>
           <Center>
             <Image
-              rounded={"lg"}
-              height={200}
-              width={200}
-              borderRadius="50%"
-              objectFit={"cover"}
-              src={IMAGE}
+              boxSize={200}
+              src={"/assets/images/michael.jpeg"}
               alt="image"
+              layout="fill"
+              objectFit={"cover"}
+              borderRadius="full"
             />
           </Center>
         </Box>
