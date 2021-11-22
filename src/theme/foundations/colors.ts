c583058dec6763fc030f5e0f7a8de1c91cb2b403
nadam131 <nickadam131@gmail.com>
2021-11-22 20:01:14 +0300

    add new colors

--- src/theme/foundations/colors.ts
+++ src/theme/foundations/colors.ts
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
   colors: {
+    background: { 800: "#000000a8" },
     primary: { 500: "#ffa819" },
+    border: { dark: "#00000020" },
   },
 };
